cb2e75fcb08020ac0d4d73dc89a85f3ecbb9b423
zrgt <garmaev@gmx.de>
2024-05-14 15:18:01 +0200

    Remove KEY_TYPES_CLASSES in __init__.py

--- basyx/aas/model/__init__.py
+++ basyx/aas/model/__init__.py
@@ -14,28 +14,3 @@ from .submodel import *
 from .provider import *
 from .concept import ConceptDescription
 from . import datatypes
-
-# A mapping of BaSyx Python SDK implementation classes to the corresponding `KeyTypes` enum members for all classes
-# that are covered by this enum.
-KEY_TYPES_CLASSES: Dict[Type[Referable], KeyTypes] = {
-    AssetAdministrationShell: KeyTypes.ASSET_ADMINISTRATION_SHELL,
-    ConceptDescription: KeyTypes.CONCEPT_DESCRIPTION,
-    Submodel: KeyTypes.SUBMODEL,
-    Entity: KeyTypes.ENTITY,
-    BasicEventElement: KeyTypes.BASIC_EVENT_ELEMENT,
-    EventElement: KeyTypes.EVENT_ELEMENT,  # type: ignore
-    Blob: KeyTypes.BLOB,
-    File: KeyTypes.FILE,
-    Operation: KeyTypes.OPERATION,
-    Capability: KeyTypes.CAPABILITY,
-    Property: KeyTypes.PROPERTY,
-    MultiLanguageProperty: KeyTypes.MULTI_LANGUAGE_PROPERTY,
-    Range: KeyTypes.RANGE,
-    ReferenceElement: KeyTypes.REFERENCE_ELEMENT,
-    DataElement: KeyTypes.DATA_ELEMENT,  # type: ignore
-    SubmodelElementCollection: KeyTypes.SUBMODEL_ELEMENT_COLLECTION,
-    SubmodelElementList: KeyTypes.SUBMODEL_ELEMENT_LIST,
-    AnnotatedRelationshipElement: KeyTypes.ANNOTATED_RELATIONSHIP_ELEMENT,
-    RelationshipElement: KeyTypes.RELATIONSHIP_ELEMENT,
-    SubmodelElement: KeyTypes.SUBMODEL_ELEMENT,  # type: ignore
-}
